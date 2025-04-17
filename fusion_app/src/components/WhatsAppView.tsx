8c869a88cebbbacf24bd5492c1fedb0a590f1ea4
danvoulez <dan@danvoulez.com>
2025-04-17 02:52:51 +0100

    t4hrhrh2456h6hy

--- fusion_app/src/components/WhatsAppView.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const WhatsAppView = () => {
-  return (
-    <div className="h-full w-full flex items-center justify-center bg-green-500 text-white">
-      <h1>WhatsApp View</h1>
-    </div>
-  )
-}
